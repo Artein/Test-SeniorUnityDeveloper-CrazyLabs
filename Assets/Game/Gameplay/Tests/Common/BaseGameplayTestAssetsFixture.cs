96171ba8ebc01d4339be70af13b2ab2bfe888bf6
Artem Sukhliak <artem.sukhliak@proton.me>
2026-06-30 13:58:42 +0300

    Restrict gameplay test assets fixture to editor platforms

--- Assets/Game/Gameplay/Tests/Common/BaseGameplayTestAssetsFixture.cs
+++ Assets/Game/Gameplay/Tests/Common/BaseGameplayTestAssetsFixture.cs
@@ -1,7 +1,13 @@
 using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
 
 namespace Game.Gameplay.Tests.Common
 {
+    [UnityPlatform(
+        RuntimePlatform.OSXEditor,
+        RuntimePlatform.WindowsEditor,
+        RuntimePlatform.LinuxEditor)]
     public abstract class BaseGameplayTestAssetsFixture
     {
         protected GameplayTestAssetsProvider TestAssets { get; private set; }
